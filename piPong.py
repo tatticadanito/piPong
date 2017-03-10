a89035fac4ad904fafe1a47a2b9f18b9bbd2c335
tatticadanito <tatticadanito99@gmail.com>
2017-03-10 15:07:40 +0100

    Added main()

--- piPong.py
+++ piPong.py
@@ -1,31 +1,21 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import sys
-
-led = [14, 15, 18, 23, 25, 8, 7, 12, 16, 20]
-bottoni = [2, 21]
-
-GPIO.setmode(GPIO.BCM)
-for init in led:
-    GPIO.setup(init, GPIO.OUT)
-for init2 in bottoni:
-    GPIO.setup(init2, GPIO.IN)
-GPIO.setwarnings(False)
-
+ 
 def andata():
     for i in led:
         GPIO.output(i, 1)
-    	sleep(0.2)
-    	GPIO.output(i, 0)
-    	sleep(0.2)
-
+        sleep(0.2)
+        GPIO.output(i, 0)
+        sleep(0.2)
+ 
 def ritorno():
     for i in led[::-1]:
         GPIO.output(i, 1)
-    	sleep(0.2)
-    	GPIO.output(i, 0)
-    	sleep(0.2)
-
+        sleep(0.2)
+        GPIO.output(i, 0)
+        sleep(0.2)
+ 
 def vittoriaRosso():
     for time in 5:
         for init in led:
@@ -34,7 +24,7 @@ def vittoriaRosso():
         for init in led:
             GPIO.output(init, 0)
         sleep(0.2)
-
+ 
 def vittoriaVerde():
     for time in 5:
         for init in led:
@@ -43,16 +33,31 @@ def vittoriaVerde():
         for init in led:
             GPIO.output(init, 0)
         sleep(0.2)
-
-continua = True
-while(continua):
-    if(bottoni[1] == 1 and led[led.lenght] == 1):
-        ritorno()
-        if(bottoni[0] == 1 and led[0] == 1):
-            andata()
+ 
+def main():
+    #--GPIO--#
+    led = [14, 15, 18, 23, 25, 8, 7, 12, 16, 20]
+    bottoni = [2, 21]
+    #--INIT--#
+    GPIO.setmode(GPIO.BCM)
+    for init in led:
+        GPIO.setup(init, GPIO.OUT)
+    for init2 in bottoni:
+        GPIO.setup(init2, GPIO.IN)
+    GPIO.setwarnings(False)
+    #--GAME--#
+    continua = True
+    while(continua):
+        if(bottoni[1] == 1 and led[led.lenght] == 1):
+            ritorno()
+            if(bottoni[0] == 1 and led[0] == 1):
+                andata()
+            else:
+                vittoriaRosso()
+                continua = False
         else:
-            vittoriaRosso()
+            vittoriaVerde()
             continua = False
-    else:
-        vittoriaVerde()
-        continua = False
+           
+if __name__ == "__main__":
+    main()
